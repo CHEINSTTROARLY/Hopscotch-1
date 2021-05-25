165adc7c75577a126f9a3d38195b516435f0294a
Jonathan Pappas <jonnyboypappas@gmail.com>
2021-05-25 09:16:38 -0700

    init

---
+++ AnotherHopscotch/AnotherHopscotch/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  AnotherHopscotch
+//
+//  Created by Jonathan Pappas on 5/25/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+}
---
+++ AnotherHopscotch/AnotherHopscotch/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AnotherHopscotch
+//
+//  Created by Jonathan Pappas on 5/25/21.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            if let scene = GameScene.init(fileNamed: "GameScene") {
+                scene.scaleMode = .aspectFit
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
